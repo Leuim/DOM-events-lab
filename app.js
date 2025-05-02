7829d9f87a512e0980d3984223e053703750560b
leuim <jassim.mohammed48@gmail.com>
2025-05-02 15:39:52 +0300

    changed the approach based on Ms. Zainab feedback

--- app.js
+++ app.js
@@ -2,67 +2,111 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let equationResult = 0
-let equationString = '0'
+let firstNumStr = ''
+let firstNum = 0
+let secondNum = 0
+let operator = null
+let total = null
 /*------------------------ Cached Element References ------------------------*/
-const buttons = document.querySelectorAll('.button')
-const calculator = document.querySelector('#calculator')
+const numberButtons = document.querySelectorAll('.number')
+const operatorButtons = document.querySelectorAll('.operator')
+
 const display = document.querySelector('.display')
-/*----------------------------- Event Listeners -----------------------------*/
-// buttons.forEach(button =>{
-//     button.addEventListener('click',(event) =>{
-//         console.log(event.target.innerText)
-//     })
-// })
-display.innerText = '0'
-const handleEquation = (event) => {
-    if (event.target.classList.contains('number') || event.target.classList.contains('operator')) {
-        if (checkValid(equationString)) {
-            if (display.textContent === '0') {
-                equationString = event.target.innerText
-                // console.log(equationElement)
-                display.textContent = equationString
-            } else {
-                equationString = equationString + event.target.innerText
-                // console.log(equationElement)
-                display.textContent = equationString
-            }
-        } else {
-            alert('Invalid equation')
-            restEquation()
+const equalsButton = document.querySelector('.equals')
+display.innerText = 0
+
+
+/*-------------------------------- Functions --------------------------------*/
+operatorButtons.forEach((operatorButton, i) => {
+    operatorButton.addEventListener('click', (event) => {
+        const opText = operatorButton.innerText
+        if (opText === '*') {
+            operator = '*'
+            secondNum = firstNum
+            firstNum = 0
+            firstNumStr = ''
+            handleDisplay()
+            console.log('We are mulitplying')
+        } else if (opText === '/') {
+            operator = '/'
+            secondNum = firstNum
+            firstNum = 0
+            firstNumStr = ''
+            handleDisplay()
+            console.log('We are dividing')
+        } else if (opText === '-') {
+            operator = '-'
+            secondNum = firstNum
+            firstNum = 0
+            firstNumStr = ''
+            handleDisplay()
+            console.log('We are subtracting')
+        } else if (opText === '+') {
+            operator = '+'
+            secondNum = firstNum
+            firstNum = 0
+            firstNumStr = ''
+            handleDisplay()
+            console.log('We are adding')
+        } else if (opText === 'C') {
+            console.log('clear')
+            handleClear()
         }
     }
-    if (checkValid(equationString)) {
-        if (event.target.innerText === '=') {
-            // console.log(display.textContent);
-            equationResult = eval(display.textContent)
-            display.innerText = equationResult
-            equationString = equationResult
-        }
-        if (event.target.innerText === 'C') {
-            restEquation()
-        }
-    } else {
-        alert('Invalid equation')
-        restEquation()
+    )
+})
+
+numberButtons.forEach((number, i) => {
+    number.addEventListener('click', (event) => {
+        firstNumStr = firstNumStr + event.target.innerText
+        firstNum = Number(firstNumStr)
+        // console.log(firstNum);
+        handleDisplay()
+    })
+})
+
+const add = () => {
+    return firstNum + secondNum
+}
+const subtract = () => {
+    return secondNum - firstNum
+}
+const divide = () => {
+    return secondNum - firstNum
+}
+const multiply = () => {
+    return firstNum * secondNum
+}
+const handleClear = () => {
+    firstNum = 0
+    secondNum = 0
+    firstNumStr = ''
+    operator = null
+    handleDisplay()
+}
+const handleDisplay = () => {
+    display.innerText = firstNum
+}
+const handleCalculate = () => {
+    if (operator === '+') {
+        total = add()
+    } else if (operator === '-') {
+        total = subtract()
+    } else if (operator === '*') {
+        total = multiply()
+    } else if (operator === '/') {
+        total = divide()
     }
 
+    firstNum = total
+    firstNumStr = total.toString()
+    secondNum = 0
+    operator = null
+    display.innerText = total
 }
-calculator.addEventListener('click', handleEquation)
-/*-------------------------------- Functions --------------------------------*/
-const restEquation = () => {
-    equationString = '0'
-    display.innerText = '0'
-}
-const checkValid = (equation) => {
-    const operators = ['*', '/', '+', '-'];
-    for (let i = 0; i < equation.length - 1; i++) {
-        if (operators.includes(equation[i]) && operators.includes(equation[i + 1])) {
-            // console.log('false');
-            return false;
-        }
-    }
-    // console.log(true);
-    return true;
-};
-// checkValid('9+9')
+
+/*----------------------------- Event Listeners -----------------------------*/
+equalsButton.addEventListener('click', handleCalculate)
+// operatorButtons.addEventListener('click', handleOperator)
+
+// numberButtons.addEventListener('click', handleNumber)
